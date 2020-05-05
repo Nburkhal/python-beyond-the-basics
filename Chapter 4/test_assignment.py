32acbc296776c80ea78a6f245eca0c419106ad52
Nburkhal <nburkhal@outlook.com>
2020-05-05 05:57:19 -0700

    assignment tests added

---
+++ Chapter 4/test_assignment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from assignment2 import LogFile, DelimFile
+
+
+log = LogFile('log.txt')                  # passes the filename to write to
+mydelim = DelimFile('data.csv', ',')      # passes the filename to write to
+                                          # and a delimeter
+
+log.write('this is a log message')        # writes a message to the file
+log.write('this is another log message')  # same
+
+mydelim.write(['a', 'b', 'c', 'd'])       # writes a list of values separated
+                                          # by comma to the file
+mydelim.write(['1', '2', '3', '4'])       # same
