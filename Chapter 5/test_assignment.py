688ff710561bf42c8f4764cf6b0bce2ec7c7ea1d
Nburkhal <nburkhal@outlook.com>
2020-05-06 09:43:54 -0700

    assignment tests added

---
+++ Chapter 5/test_assignment.py
@@ -0,0 +1,11 @@
+from assignment3 import ConfigDict     # you can put class ConfigDict 
+                                       # in assignment3.py, or just put the 
+                                       # class code in the same file as this
+                                       # calling code; then this import 
+                                       # statement is not needed
+
+cc = ConfigDict('config_file.txt')
+
+print(cc['query'])                     # SELECT this FROM that WHERE condition
+print(cc['email_to'])                  # me@mydomain.com
+cc['database'] = 'mysql_managed'       # [ this writes to the config file ]
