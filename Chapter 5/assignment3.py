f1a56d4913accc1878fd87eeb43831ca78c5d6cb
Nburkhal <nburkhal@outlook.com>
2020-05-06 09:43:11 -0700

    added assignment 3

---
+++ Chapter 5/assignment3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+
+class ConfigDict(dict):
+
+    def __init__(self, filename):
+
+        self._filename = filename
+        if os.path.isfile(self._filename):
+            with open(self._filename) as fh:
+                for line in fh:
+                    line = line.rstrip()
+                    key, value = line.split('=', 1)
+                    dict.__setitem__(self, key, value)
+
+    def __setitem__(self, key, value):
+
+        dict.__setitem__(self, key, value)
+        with open(self._filename, 'w') as fh:
+            for key, value in self.items():
+                fh.write(f"{key}={value}")
